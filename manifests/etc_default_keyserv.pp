11b22c6d5c9766072b5fd77f0e69fe8b07bce69e
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-19 22:52:45 +0800

    go back to file_line?

--- manifests/etc_default_keyserv.pp
+++ manifests/etc_default_keyserv.pp
@@ -3,11 +3,17 @@
 # Disable 'nobody' keys in /etc/default/keyserv
 class solaris_tidy::etc_default_keyserv {
 
-  shellvar { "/etc/default/keyserv ENABLE_NOBODY_KEYS":
-    ensure   => present,
-    variable => "ENABLE_NOBODY_KEYS",
-    target   => "/etc/default/keyserv",
-    value    => "NO",
+  # Seems to crash if all lines commented for some reason... sigh... file_line
+  # shellvar { "/etc/default/keyserv ENABLE_NOBODY_KEYS":
+  #   ensure   => present,
+  #   variable => "ENABLE_NOBODY_KEYS",
+  #   target   => "/etc/default/keyserv",
+  #   value    => "NO",
+  # }
+  file_line { "/etc/default/keyserv ENABLE_NOBODY_KEYS":
+    ensure => present,
+    line   => "ENABLE_NOBODY_KEYS=NO",
+    path   => "/etc/default/keyserv",
+    match  => "^ENABLE_NOBODY_KEYS",
   }
-
 }
