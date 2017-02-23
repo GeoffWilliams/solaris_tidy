0e510d22f14ae48e685c9f9d2f90f82b1593a2f9
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-24 01:56:23 +1100

    fix shellvar that isn't a shellvar, release

--- manifests/etc_security_policy.pp
+++ manifests/etc_security_policy.pp
@@ -4,11 +4,22 @@
 class solaris_tidy::etc_security_policy {
 
   # Set Retry Limit for Account Lockout
-  shellvar { "/etc/security/policy.conf LOCK_AFTER_RETRIES":
-    ensure   => present,
-    variable => "LOCK_AFTER_RETRIES",
-    target   => "/etc/security/policy.conf ",
-    value    => "YES",
+
+  # The target file seems to follow the solaris trick of using a file that looks
+  # like simple shell variables but actually isn't.  In a stock opensolaris
+  # /etc/secuirty/policy.conf file there is a line where a variable is assigned
+  # that contains spaces and is not quoted:
+  #   PROFS_GRANTED=Basic Solaris User
+  # this is an error in SH and results in a variable PROFS_GRANTED containing
+  # 'Basic' followed by an attempt to execute the commands 'Solaris' and 'User'.
+  # This also causes the shellvar augeas type (correctly) to fail with an error
+  # about the lens not matching.  For simplicity, rewrite this change to use a
+  # file_line resource
+  file_line { "/etc/security/policy.conf LOCK_AFTER_RETRIES":
+    ensure => present,
+    line   => "LOCK_AFTER_RETRIES=YES",
+    path   => "/etc/security/policy.conf",
+    match  => "^LOCK_AFTER_RETRIES=",
   }
 
 }
